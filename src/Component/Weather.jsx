92054ed0407b679e3728292cbc3594ff643ae682
DarshanPawar23 <darshansp0230@gmail.com>
2025-09-11 22:59:30 +0530

    final

--- src/Component/Weather.jsx
+++ src/Component/Weather.jsx
@@ -9,77 +9,86 @@ import rain_icon from '../assets/rain.png'
 import snow_icon from '../assets/snow.png'
 import wind_icon from '../assets/wind.png'
 function Weather() {
-  const[weather , Setweather] =useState(false)
+  const [weather, Setweather] = useState(false)
   const ref = useRef();
-  const allicon ={
-    "01d" :clear_icon,
-    "01n" :clear_icon,
-    "02d" :cloud_icon,
-    "02n" :cloud_icon,
-    "03d" :clear_icon,
-    "03n" :clear_icon,
-    "04d" :drizzle_icon,
-    "04n" :drizzle_icon,
-    "09d" :rain_icon,
-    "09n" :rain_icon,
-    "10d" :rain_icon,
-    "10n" :rain_icon,
-    "13d" :snow_icon,
-    "13n" :snow_icon,
+  const allicon = {
+    "01d": clear_icon,
+    "01n": clear_icon,
+    "02d": cloud_icon,
+    "02n": cloud_icon,
+    "03d": clear_icon,
+    "03n": clear_icon,
+    "04d": drizzle_icon,
+    "04n": drizzle_icon,
+    "09d": rain_icon,
+    "09n": rain_icon,
+    "10d": rain_icon,
+    "10n": rain_icon,
+    "13d": snow_icon,
+    "13n": snow_icon,
   }
-  const search = async(city)=>{
-    if(city===""){
+  const search = async (city) => {
+    if (city === "") {
       alert("Enter city name");
       return;
     }
-    try{
-       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
 
-       const response = await fetch(url);
-       const data = await response.json();
-       if(!response.ok){
+      const response = await fetch(url);
+      const data = await response.json();
+      if (!response.ok) {
         alert("city not found")
-       }
-       console.log(data)
-       const icon = allicon[data.weather[0].icon] || clear_icon;
+      }
+      console.log(data)
+      const icon = allicon[data.weather[0].icon] || clear_icon;
 
-       Setweather({
-        humidity : data.main.humidity,
-        temprature : Math.floor(data.main.temp),
-        windSpeed : data.wind.speed,
-         location : data.name,
-         icon : icon
-       })
+      Setweather({
+        humidity: data.main.humidity,
+        temprature: Math.floor(data.main.temp),
+        windSpeed: data.wind.speed,
+        location: data.name,
+        icon: icon
+      })
     }
-    catch{
+    catch {
       Setweather(false)
     }
   }
-  useEffect(()=>{
-   search("London")
-  },[])
+  useEffect(() => {
+    search("London")
+  }, [])
   return (
     <div className='weather'>
-        <div className="search-bar">
-            <input type='text' placeholder='search' ref={ref}/>
-            <img src={search_icon} alt='search_icon' onClick={()=>search(ref.current.value)}/>
-        </div>
-        { weather?<>
-        <img src={weather.icon}  alt='clear_icon' className='weather-icon'/>
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="search"
+          ref={ref}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              search(ref.current.value);
+            }
+          }}
+        />
+        <img src={search_icon} alt='search_icon' onClick={() => search(ref.current.value)} />
+      </div>
+      {weather ? <>
+        <img src={weather.icon} alt='clear_icon' className='weather-icon' />
         <p className='temprature'>{weather.temprature}°c</p>
         <p className='location'>{weather.location}</p>
 
         <div className="weather-data">
           <div className="col">
-            <img src={wind_icon} alt='wind-icon'/>
+            <img src={wind_icon} alt='wind-icon' />
             <div>
               <p>{weather.windSpeed}km/h</p>
               <span>Wind Speed</span>
             </div>
           </div>
 
-           <div className="col">
-            <img src={humidity_icon} alt='humidity-icon'/>
+          <div className="col">
+            <img src={humidity_icon} alt='humidity-icon' />
             <div>
               <p>{weather.humidity}%</p>
               <span>Humidity</span>
